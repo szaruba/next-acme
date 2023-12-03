40061c7a8b83393a2bf1a51fbd5c923413d1a7bd
Stefan Zaruba <stefan.zaruba.dev@gmail.com>
2023-12-03 23:06:14 +0100

    some changes ;)

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function InvoicesPage() {
+  return <p>Some expensive invoices</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardPage() {
+  return <>Welcome to the Däshboard</>;
+}
